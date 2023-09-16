424ec84009e48d3edfc6dde2ca8cedd2aa5a146b
George Mathews <george@georgemathe.ws>
2023-09-16 16:57:19 -0700

    Change nvm to fnm

--- init.sh
+++ init.sh
@@ -6,12 +6,6 @@ if ! command -v brew &> /dev/null; then
     exit 1
 fi
 
-# Check if Node.js is installed
-if ! command -v node &> /dev/null; then
-    echo "Node.js is not installed. Please install Node.js(https://github.com/nvm-sh/nvm) before continuing."
-    exit 1
-fi
-
 # Define an associative array to store relative source file paths and their corresponding target paths
 declare -A symlinks
 
@@ -48,6 +42,10 @@ done
 
 echo "All symbolic links created successfully."
 
+# # Install node
+# brew install fnm
+# fnm install 16
+#
 # # Install plugin manager for neovim
 # sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 #        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
