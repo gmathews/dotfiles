75891df7725c49b737d0aaeec28695b4b9dcc626
George Mathews <george@georgemathe.ws>
2026-04-11 15:09:48 -0700

    use lazy.nvim instead of plug.vim

--- init.sh
+++ init.sh
@@ -47,12 +47,7 @@ echo "All symbolic links created successfully."
 
 # Install node
 brew install fnm
-fnm install 20
-
-# Install plugin manager for neovim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
+fnm install 22
 
 # Install a bunch of software
 brew install git
